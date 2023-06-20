6acd3ea89530b137329387b8cfc21a83914afcaa
Jackeagle <jackeagle102@gmail.com>
2023-06-20 19:55:38 +0530

    BlissSystemUIInitializer: Log BlissSystemUIInitializer for debugging purpose
    Change-Id: I2560f2287c3c14e215de611e8481fff91ef64bec
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/org/blissroms/systemui/BlissSystemUIInitializer.kt
+++ src/org/blissroms/systemui/BlissSystemUIInitializer.kt
@@ -7,11 +7,18 @@
 package org.blissroms.systemui
 
 import android.content.Context
+import android.util.Log
 import com.android.systemui.SystemUIInitializer
 import com.android.systemui.dagger.GlobalRootComponent
 import org.blissroms.systemui.dagger.DaggerBlissGlobalRootComponent
 
 class BlissSystemUIInitializer(context: Context) : SystemUIInitializer(context) {
+
+    private val TAG = "BlissSystemUIInitializer"
+
+    init {
+        Log.d(TAG, "BlissSystemUIInitializer initialized")
+    }
 
     override fun getGlobalRootComponentBuilder(): GlobalRootComponent.Builder {
         return DaggerBlissGlobalRootComponent.builder()
